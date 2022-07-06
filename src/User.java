2e752610f3557ef89c0bb082ea15addd84b3b7ec
milad <miladnooraiy0@gmail.com>
2022-07-06 16:27:38 +0430

    regexes for signup are set

--- src/User.java
+++ src/User.java
@@ -16,44 +16,79 @@ public class User {
     }
 
     public static void signUp() throws SQLException {
-        String fullName = JOptionPane.showInputDialog(null ,"Please Enter Your FullName :",
-                "Sign-Up page" , JOptionPane.QUESTION_MESSAGE);
-        String userName = JOptionPane.showInputDialog(null ,"Please Enter Your username :",
-                "Sign-Up page" , JOptionPane.QUESTION_MESSAGE);
-        String passWord = JOptionPane.showInputDialog(null ,"Please Enter your passWord :",
-                "Sign-Up Page" , JOptionPane.QUESTION_MESSAGE);
-        String email = JOptionPane.showInputDialog(null ,"Please Enter Your Email :",
-                "Sign-Up page" , JOptionPane.QUESTION_MESSAGE);
+        String userName =null;
+        String fullName = null;
+        String passWord = null;
+        String email = null;
+        while (true) {
+            fullName = JOptionPane.showInputDialog(null, "Please Enter Your FullName :",
+                    "Sign-Up page", JOptionPane.QUESTION_MESSAGE);
+            if ((fullName != null) && (!(fullName.length() == 0)) && (fullName.matches("^[a-zA-Z]*$"))) {
+                break;
+            } else {
+                JOptionPane.showInputDialog(null, "Not accepted ! Please try another one",
+                        "Sign-Up page", JOptionPane.QUESTION_MESSAGE);
+            }
+        }
+        while (true) {
+            userName = JOptionPane.showInputDialog(null, "Please Enter Your username :",
+                    "Sign-Up page", JOptionPane.QUESTION_MESSAGE);
+            if ((userName != null) && ((userName.length() > 4))) {
+                break;
+            } else {
+                JOptionPane.showInputDialog(null, "Not accepted ! Please try another one",
+                        "Sign-Up page", JOptionPane.QUESTION_MESSAGE);
+            }
+        }
+        while (true){
+            passWord = JOptionPane.showInputDialog(null, "Please Enter your passWord :",
+                    "Sign-Up Page", JOptionPane.QUESTION_MESSAGE);
+            if ((passWord != null) && ((passWord.length() > 4))) {
+                break;
+            } else {
+                JOptionPane.showInputDialog(null, "Not accepted ! Please try another one",
+                        "Sign-Up page", JOptionPane.QUESTION_MESSAGE);
+            }
+        }
+        while (true){
+            email = JOptionPane.showInputDialog(null, "Please Enter Your Email :",
+                    "Sign-Up page", JOptionPane.QUESTION_MESSAGE);
+            if ((email != null) && (!(email.length() == 0)) && (email.matches("^[A-Za-z0-9+_.-]+@(.+)$"))) {
+                break;
+            } else {
+                JOptionPane.showInputDialog(null, "Not accepted ! Please try another one",
+                        "Sign-Up page", JOptionPane.QUESTION_MESSAGE);
+            }
+        }
 
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/trello?" +
                 "autoReconnect=true&useSSL=false", "root", "");
 
-       PreparedStatement preparedStatement = connection.prepareStatement("Select * from Users where username = ? " +
-               "or email = ?");
-       preparedStatement.setString(1, userName);
-       preparedStatement.setString(2, email);
-       ResultSet resultSet = preparedStatement.executeQuery();
-       try {
-           if (resultSet.next()) {
-               JOptionPane.showMessageDialog(null, "This User Name or Email has Been" +
-                       " Chosen Before", "Sign-Up page", JOptionPane.INFORMATION_MESSAGE);
+        PreparedStatement preparedStatement = connection.prepareStatement("Select * from Users where username = ? " +
+                "or email = ?");
+        preparedStatement.setString(1, userName);
+        preparedStatement.setString(2, email);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        try {
+            if (resultSet.next()) {
+                JOptionPane.showMessageDialog(null, "This User Name or Email has Been" +
+                        " Chosen Before", "Sign-Up page", JOptionPane.INFORMATION_MESSAGE);
 
-           }
-           else {
-               PreparedStatement preparedStatement2 = connection.prepareStatement("Insert into Users values(?, ?, ?, ?, ?)");
-               preparedStatement2.setString(1, userName);
-               preparedStatement2.setString(2, passWord);
-               preparedStatement2.setString(3, email);
-               preparedStatement2.setString(4, fullName);
-               preparedStatement2.setString(5, "");
-               preparedStatement2.executeUpdate();
-               JOptionPane.showMessageDialog(null, "Your Account Was Registered " +
-                       "Successfully" ,"Sign-Up page",JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                PreparedStatement preparedStatement2 = connection.prepareStatement("Insert into Users values(?, ?, ?, ?, ?)");
+                preparedStatement2.setString(1, userName);
+                preparedStatement2.setString(2, passWord);
+                preparedStatement2.setString(3, email);
+                preparedStatement2.setString(4, fullName);
+                preparedStatement2.setString(5, "");
+                preparedStatement2.executeUpdate();
+                JOptionPane.showMessageDialog(null, "Your Account Was Registered " +
+                        "Successfully", "Sign-Up page", JOptionPane.INFORMATION_MESSAGE);
 
-           }
-       }catch(Exception err){
-           System.out.println("err");
-       }
+            }
+        } catch (Exception err) {
+            System.out.println("err");
+        }
 
     }
     public static User logIn() throws SQLException {
