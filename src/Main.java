7a8065507fec27b57d275d38c0547a5fd58632d3
milad <miladnooraiy0@gmail.com>
2022-07-06 13:41:01 +0430

    started creating main page

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
+import javax.swing.*;
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        User.logIn();
+        JOptionPane.showMessageDialog(null, "--- Welcome To Mini Trello ---",
+                "Welcome", JOptionPane.INFORMATION_MESSAGE);
+
+        boolean isAppRunning = true;
+        while (isAppRunning) {
+            int chosenOption = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Please choose an option :\n 1. Log-In\n 2. Sign-Up\n 3. Exit", "Log-In/Sign-Up Page"
+                    , JOptionPane.QUESTION_MESSAGE));
+            switch (chosenOption) {
+                case 1:
+                    User currentUser = User.logIn();
+                    boolean isInHomePage = true;
+                    while (isInHomePage) {
+                        while (isInHomePage) {
+                            chosenOption = Integer.parseInt(JOptionPane.showInputDialog(null,
+                                    "Please choose an option :\n1.WorkSpaces\n2. Edit Profile\n 3.Log Out",
+                                    "Home page", JOptionPane.QUESTION_MESSAGE));
+                            switch (chosenOption) {
+                                case 1:
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    isInHomePage = false;
+                                    break;
+
+
+                            }
+
+                        }
+                    }
+            }
+
+        }
     }
 }
