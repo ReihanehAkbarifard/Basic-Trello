1a0b631fb16a1717ea8a9948dbc218af3cb31a0b
Reihaneh Akbarifard <107428087+ReihanehAkbarifard@users.noreply.github.com>
2022-07-06 12:45:58 +0430

    login page was added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-
+        User.logIn();
     }
 }
--- src/User.java
+++ src/User.java
@@ -15,7 +15,7 @@ public class User {
         this.email = email;
     }
 
-    public static User signUp() throws SQLException {
+    public static void signUp() throws SQLException {
         String fullName = JOptionPane.showInputDialog(null ,"Please Enter Your FullName :",
                 "Sign-Up page" , JOptionPane.QUESTION_MESSAGE);
         String userName = JOptionPane.showInputDialog(null ,"Please Enter Your username :",
@@ -33,26 +33,60 @@ public class User {
        preparedStatement.setString(1, userName);
        preparedStatement.setString(2, email);
        ResultSet resultSet = preparedStatement.executeQuery();
-       boolean isRegistered = false;
-       if(resultSet.next()){
-           JOptionPane.showMessageDialog(null, "This User Name or Email has Been" +
-                           " Chosen Before" ,"Sign-Up page",JOptionPane.INFORMATION_MESSAGE);
-           return null;
-       }
-       else {
-           PreparedStatement preparedStatement2 = connection.prepareStatement("Insert into Users values(?, ?, ?, ?, ?)");
-           preparedStatement2.setString(1, userName);
-           preparedStatement2.setString(2, passWord);
-           preparedStatement2.setString(3, email);
-           preparedStatement2.setString(4, fullName);
-           preparedStatement2.setString(5, "");
-           preparedStatement2.executeUpdate();
-           User newUser = new User(fullName ,userName ,passWord, email);
-           JOptionPane.showMessageDialog(null, "Your Account Was Registered " +
-                   "Successfully" ,"Sign-Up page",JOptionPane.INFORMATION_MESSAGE);
-           return newUser;
+       try {
+           if (resultSet.next()) {
+               JOptionPane.showMessageDialog(null, "This User Name or Email has Been" +
+                       " Chosen Before", "Sign-Up page", JOptionPane.INFORMATION_MESSAGE);
+
+           }
+           else {
+               PreparedStatement preparedStatement2 = connection.prepareStatement("Insert into Users values(?, ?, ?, ?, ?)");
+               preparedStatement2.setString(1, userName);
+               preparedStatement2.setString(2, passWord);
+               preparedStatement2.setString(3, email);
+               preparedStatement2.setString(4, fullName);
+               preparedStatement2.setString(5, "");
+               preparedStatement2.executeUpdate();
+               JOptionPane.showMessageDialog(null, "Your Account Was Registered " +
+                       "Successfully" ,"Sign-Up page",JOptionPane.INFORMATION_MESSAGE);
+
+           }
+       }catch(Exception err){
+           System.out.println("err");
        }
 
     }
+    public static User logIn() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/trello?" +
+                "autoReconnect=true&useSSL=false", "root", "");
+        Boolean isFound = false;
+        do {
+            String userName = JOptionPane.showInputDialog(null ,"Please Enter Your username :",
+                    "logIn page" , JOptionPane.QUESTION_MESSAGE);
+            String passWord = JOptionPane.showInputDialog(null ,"Please Enter your passWord :",
+                    "logIn Page" , JOptionPane.QUESTION_MESSAGE);
+            PreparedStatement preparedStatement = connection.prepareStatement("Select * from Users where username = ? " +
+                    "and password = ?");
+            preparedStatement.setString(1, userName);
+            preparedStatement.setString(2, passWord);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                JOptionPane.showMessageDialog(null, "Welcome Dear " +
+                        resultSet.getString("fullname"), "Sign-Up page",
+                        JOptionPane.INFORMATION_MESSAGE);
+                User user = new User(resultSet.getString("fullname"),
+                        resultSet.getString("userID"), resultSet.getString("password") ,
+                        resultSet.getString("email"));
+                return user;
+
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "User Not Found"
+                        ,"Log-Up page",JOptionPane.INFORMATION_MESSAGE);
+            }
+        }while (true);
+
+    }
+
 
 }
