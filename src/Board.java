c24274943e9a60792b35a351b447dd58c4f8c214
Reihaneh Akbarifard <107428087+ReihanehAkbarifard@users.noreply.github.com>
2022-07-07 16:06:50 +0430

    creat workspace and boards classes

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,7 @@
+import java.util.ArrayList;
+
 public class Board {
+    private String title;
+    private ArrayList<User> guests = new ArrayList<>();
+    private ArrayList<User> admins = new ArrayList<>();
 }
--- src/WorkSpace.java
+++ src/WorkSpace.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+
 public class WorkSpace {
+    private String workSpaceName;
+    private ArrayList<User> guests = new ArrayList<>();
+    private ArrayList<User> admins = new ArrayList<>();
+    private ArrayList<User> members = new ArrayList<>();
+    private ArrayList<Board> boards = new ArrayList<>();
+    private String status;
+    private String type;
 
 }
